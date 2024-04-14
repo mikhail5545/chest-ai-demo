0e0261ee46885a1e0bb23abdbce3d12d64a1d555
Mikhail Kulik <67920426+mikhail5545@users.noreply.github.com>
2024-04-14 17:47:08 +0300

    Update NeuralModel.py
    Sigmoid activation function was replaced by tanh activation function in cause of better accuracy.

--- src/core/NeuralModel.py
+++ src/core/NeuralModel.py
@@ -11,7 +11,7 @@ def neural_model(train_x: pd.DataFrame) -> tf.keras.models.Sequential:
             core_model.add(tf.keras.layers.Dropout(0.5))
             core_model.add(tf.keras.layers.Dense(512))
             core_model.add(tf.keras.layers.Dropout(0.5))
-            core_model.add(tf.keras.layers.Dense(15, activation='sigmoid'))
+            core_model.add(tf.keras.layers.Dense(15, activation='tanh'))
             core_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy', 'mse'])
     except RuntimeError as e:
         print(e)
